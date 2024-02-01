fc42e5049dfac0bb04f215bae2796a211e4c1538
acidicneko <ayush7788899@gmail.com>
2024-02-01 09:49:43 +0530

    add: update function in gitman script

--- gitman_install.sh
+++ gitman_install.sh
@@ -9,11 +9,16 @@ uninstall() {
 	rm "$GITMAN_BIN"/rci
 }
 
+update() {
+	install
+}
 
 if [ "$1" = "install" ] ; then
 	install
 elif [ "$1" = "uninstall" ] ; then
 	uninstall
+elif [ "$1" = "update" ] ; then
+	update
 else
 	echo "unknown option"
 	exit 1
